5e79c8262ec996c2728ba0d090cab103e5e2b821
Ivan Durlen <idurlen@foi.hr>
2015-11-18 18:15:00 +0100

    First commit.

---
+++ EGrader/Models/UsersModel.cs
@@ -0,0 +1,33 @@
+﻿using EGrader.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EGrader.Models {
+    class UsersModel {
+
+        private DatabaseManager databaseManager;
+
+        private string tableName = "users";
+
+        public UsersModel(DatabaseManager databaseManager) {
+            this.databaseManager = databaseManager;
+        }
+
+
+        public bool userExists(String username) {
+            List<List<String>> existentUser = databaseManager.Select("SELECT id FROM users WHERE username = '" + username +"' ");
+            return existentUser.Capacity > 0;
+        }
+
+        public bool userExists(String username, String password) {
+            String query = "SELECT id FROM users WHERE username = '" + username + "' AND password = '" + password + "' ";
+            List<List<String>> existentUser = databaseManager.Select(query);
+            return existentUser.Capacity > 0;
+        }
+
+
+    }
+}
---
+++ EGrader/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using EGrader.Classes;
+using EGrader.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EGrader.Windows {
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window {
+
+        int invalidCredentialsCounter = 0;
+        UsersModel usersModel = new UsersModel(DatabaseManager.GetInstance());
+
+        // ---------- TEMPORARY ---------- 
+        String[,] usersPasswords = new String[,] { { "idurlen", "fnh57dsx" }, { "admin", "AdmiN12345" } };
+        // -------------------------------
+
+
+        public LoginWindow() {
+            InitializeComponent();
+            buttonLogin.IsEnabled = false;
+        }
+
+
+        private void HandleKeyPress(object sender, RoutedEventArgs e) {
+            if (IsExistentUser(textBoxUsername.Text))
+                buttonLogin.IsEnabled = true;
+        }
+
+
+        private void HandleLogin(object sender, RoutedEventArgs e) {
+            if (!isValidCredentials(textBoxUsername.Text, textBoxPassword.Password)) {
+                invalidCredentialsCounter++;
+                if (invalidCredentialsCounter == 3)
+                    labelForgottenPassword.Visibility = Visibility.Visible;
+            }
+            else {
+                MessageBox.Show("Valid login for: " + textBoxUsername.Text);
+                MainWindow mainWindow = new MainWindow();
+                App.Current.MainWindow = mainWindow;
+                this.Close();
+                mainWindow.Show();
+            }
+        }
+
+
+        private Boolean IsExistentUser(String username) {
+            return usersModel.userExists(username);
+        }
+
+
+        private Boolean isValidCredentials(String username, String password) {
+            return usersModel.userExists(username, password);
+        }
+    }
+}
